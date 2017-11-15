dc1fc4661db138cac783fbe689ed0de5b70f5620
myasin89 <cuyasin@gmail.com>
2017-11-15 18:41:16 +0500

    Added Offline mode functionality

--- app/src/main/java/com/cto247/directoryapp/models/EmployeeInfo.java
+++ app/src/main/java/com/cto247/directoryapp/models/EmployeeInfo.java
@@ -1,13 +1,17 @@
 package com.cto247.directoryapp.models;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Comparator;
 
 /**
  * Created by Sam on 10/7/2017.
  */
-
+@Entity
 public class EmployeeInfo {
-
+    @PrimaryKey(autoGenerate = true)
+    private Long Id;
     private String EmployeeId;
     private String EmployeeName;
     private String FullName;
@@ -16,38 +20,55 @@ public class EmployeeInfo {
     private String ContactNumber;
     private String EmergencyContactNumber;
     private String Address;
+    private String Mobile;
+
+    public Long getId(){ return Id; }
+    public void setId(Long value){ Id = value; }
 
     public String getEmployeeId() {
         return EmployeeId;
     }
+    public void setEmployeeId(String employeeId){ EmployeeId = employeeId; }
 
     public String getEmployeeName() {
         return EmployeeName;
     }
+    public void setEmployeeName(String employeeName){ EmployeeName = employeeName; }
 
     public String getFullName() {
         return FullName;
     }
+    public void setFullName(String fullName){ FullName = fullName; }
+
+    public String getMobile() {
+        return Mobile;
+    }
+    public void setMobile(String mobile){ Mobile = mobile; }
 
     public String getDepartment() {
         return Department;
     }
+    public void setDepartment(String department){ Department = department; }
 
     public String getExt() {
         return Ext;
     }
+    public void setExt(String ext){ Ext = ext; }
 
     public String getContactNumber() {
         return ContactNumber;
     }
+    public void setContactNumber(String contactNumber){ ContactNumber = contactNumber; }
 
     public String getEmergencyContactNumber() {
         return EmergencyContactNumber;
     }
+    public void setEmergencyContactNumber(String emergencyContactNumber){ EmergencyContactNumber = emergencyContactNumber; }
 
     public String getAddress() {
         return Address;
     }
+    public void setAddress(String address){ Address = address; }
 
     public static Comparator<EmployeeInfo> CompareByName = new Comparator<EmployeeInfo>(){
         public int compare(EmployeeInfo e1, EmployeeInfo e2) {
